d960926b88deae5e65e463b01fe64d61b6fc3826
leo <lgimelshtein@gmail.com>
2016-02-04 12:18:01 -0400

    Update default.rb
    test

--- wpconfig/recipes/default.rb
+++ wpconfig/recipes/default.rb
@@ -1,7 +1,7 @@
-cookbook_file "/var/www/wordpress/current/wp-config.php" do
+cookbook_file "/opt/wordpress/current/wp-config.php" do
   source "wp-config.php"
   mode '0755'
   action :create
-  owner 'apache'
-  group 'apache'
+  owner 'ec2-user'
+  group 'ec2-user'
 end
