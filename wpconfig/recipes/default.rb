e9add94fd5ea30ee54d527bcdb43ddf26141a84f
leo <lgimelshtein@gmail.com>
2016-02-04 12:03:30 -0400

    Update default.rb
    change path

--- wpconfig/recipes/default.rb
+++ wpconfig/recipes/default.rb
@@ -1,7 +1,7 @@
-cookbook_file "/srv/www/wordpress/current/wp-config.php" do
+cookbook_file "/var/www/wordpress/current/wp-config.php" do
   source "wp-config.php"
   mode '0755'
   action :create
   owner 'deploy'
-  group 'www-data'
+  group 'apache'
 end
